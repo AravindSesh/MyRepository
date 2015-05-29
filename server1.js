76f506a5e87c2c03ecd9d1a47e62e080228e1445
AravindSesh <aseshad1@hawk.iit.edu>
2015-05-29 10:55:20 -0500

    Server1.js has been modified!

--- server1.js
+++ server1.js
@@ -1,6 +1,6 @@
 var http=require("http");
 http.createServer(function(req,res){
 	res.writeHead(200,{"Content-Type":"text/plain"});
-	res.write("Hello World");
+	res.write("Welcome User");
 }).listen(3000);
-console.log("Server created successfully!");
+console.log("Server has been created successfully!");
